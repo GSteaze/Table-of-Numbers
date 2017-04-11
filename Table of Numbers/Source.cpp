ca54bad768994ac49d6d31d6f8987adaf85c5827
comet <comet@WINDOWS-V94VPOF>
2017-04-10 21:48:22 -0500

    Test commented out and main reinstated

--- Table of Numbers/Source.cpp
+++ Table of Numbers/Source.cpp
@@ -12,40 +12,40 @@ const double kDefaultValue = 1002.0;
 
 int main()
 {
-	//double userInput[3];
-	//InitializeList(userInput, 3, 0.0);
-	//InputNumbers(userInput, 3);
-	//const double numberOfValues = userInput[0];
-	//const double firstNumber = userInput[1];
-	//const double incrementValue = userInput[2];
-	//TableBuilder(numberOfValues, firstNumber, incrementValue);
+	double userInput[3];
+	InitializeList(userInput, 3, 0.0);
+	InputNumbers(userInput, 3);
+	const double numberOfValues = userInput[0];
+	const double firstNumber = userInput[1];
+	const double incrementValue = userInput[2];
+	TableBuilder(numberOfValues, firstNumber, incrementValue);
 
-	//double square[kDefaultListSize];
-	//InitializeList(square, kDefaultListSize, kDefaultValue);
-	//double squareRoot[kDefaultListSize];
-	//InitializeList(squareRoot, kDefaultListSize, kDefaultValue);
-	//double cube[kDefaultListSize];
-	//InitializeList(cube, kDefaultListSize, kDefaultValue);
-	//double cubeRoot[kDefaultListSize];
-	//InitializeList(cubeRoot, kDefaultListSize, kDefaultValue);
-	//double evenOrOdd[kDefaultListSize];
-	//InitializeList(evenOrOdd, kDefaultListSize, kDefaultValue);
-	//double primeOrNot[kDefaultListSize];
-	//InitializeList(primeOrNot, kDefaultListSize, kDefaultValue);
-	//double divisors[kDefaultListSize];
-	//InitializeList(divisors, kDefaultListSize, kDefaultValue);
-	//double perfectOrNot[kDefaultListSize];
-	//InitializeList(perfectOrNot, kDefaultListSize, kDefaultValue);
-	//
-	//CalculateResults(numberOfValues, square, squareRoot, cube, cubeRoot,
-	//	evenOrOdd, primeOrNot, perfectOrNot, divisors, numberOfValues);
-	//DisplayResults(square, squareRoot, cube, cubeRoot,
-	//	evenOrOdd, primeOrNot, perfectOrNot, numberOfValues);
-	//PrintResults(square, squareRoot, cube, cubeRoot,
-	//	evenOrOdd, primeOrNot, perfectOrNot, numberOfValues);
+	double square[kDefaultListSize];
+	InitializeList(square, kDefaultListSize, kDefaultValue);
+	double squareRoot[kDefaultListSize];
+	InitializeList(squareRoot, kDefaultListSize, kDefaultValue);
+	double cube[kDefaultListSize];
+	InitializeList(cube, kDefaultListSize, kDefaultValue);
+	double cubeRoot[kDefaultListSize];
+	InitializeList(cubeRoot, kDefaultListSize, kDefaultValue);
+	double evenOrOdd[kDefaultListSize];
+	InitializeList(evenOrOdd, kDefaultListSize, kDefaultValue);
+	double primeOrNot[kDefaultListSize];
+	InitializeList(primeOrNot, kDefaultListSize, kDefaultValue);
+	double divisors[kDefaultListSize];
+	InitializeList(divisors, kDefaultListSize, kDefaultValue);
+	double perfectOrNot[kDefaultListSize];
+	InitializeList(perfectOrNot, kDefaultListSize, kDefaultValue);
+	
+	CalculateResults(numberOfValues, square, squareRoot, cube, cubeRoot,
+		evenOrOdd, primeOrNot, perfectOrNot, divisors, numberOfValues);
+	DisplayResults(square, squareRoot, cube, cubeRoot,
+		evenOrOdd, primeOrNot, perfectOrNot, numberOfValues);
+	PrintResults(square, squareRoot, cube, cubeRoot,
+		evenOrOdd, primeOrNot, perfectOrNot, numberOfValues);
 
-	//
-	TestCode();
+	
+	//TestCode();
 
 	return 0;
 }
